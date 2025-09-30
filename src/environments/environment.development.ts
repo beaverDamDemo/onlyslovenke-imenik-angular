cd0e3d171c7476e4f91bfe812cfac8c5e37d7da6
beaverDamDemo <zdenevi@gmail.com>
2025-09-30 17:19:13 +0200

    update

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     API_URL: 'https://localhost:5001',
     appVersion: '0.0.0',
-    buildDate: '2025-09-29 21:55',
+    buildDate: '2025-09-30 15:06',
     production: false,
   };
